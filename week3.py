a856f0e330066dcf5fab56ccc4f7c83ec4ce657e
Omidmohaddesi <omidmohaddesi@gmail.com>
2019-07-12 16:05:36 -0400

    finish course

---
+++ week3.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+print(tf.__version__)
+
+
+mnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+training_images = training_images.reshape(60000, 28, 28, 1)
+test_images = test_images.reshape(10000, 28, 28, 1)
+training_images, test_images = training_images / 255.0, test_images / 255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+                                    tf.keras.layers.MaxPooling2D(2, 2),
+                                    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+                                    tf.keras.layers.MaxPooling2D(2, 2),
+                                    tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation='relu'),
+                                    tf.keras.layers.Dense(10, activation='softmax')])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+model.fit(training_images, training_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(test_acc)
+# classification = model.predict(test_images)
+# print(classification[0])
+# print(test_labels[0])
